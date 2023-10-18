400b22b8c640e3a1f944d7f2f9c535ef5a08dba2
airsalvatierra <air.salvatierra@gmail.com>
2023-10-18 17:09:41 -0600

    add dosctring

--- pokemon_api.py
+++ pokemon_api.py
@@ -3,11 +3,14 @@ import requests
 BASE_PATH = 'https://pokeapi.co/api/v2/pokemon/'
 
 
+# Asking user input
 print("Welcome to an ugly pokedex")
 pokemon_name = input(
     'Enter the name of a pokemon and we will tell you more about it: '
 )
 
+# try to get a valid response from the API (right pokemon name)
+# It will keep asking for a pokemon name if keeps failing.
 response = requests.get(f'{BASE_PATH}/{pokemon_name}/')
 while response.status_code != 200:
     pokemon_name = input('That pokemon does not exits. Enter the right name: ')
@@ -15,6 +18,7 @@ while response.status_code != 200:
 
 pokemon_data = response.json()
 
+# Parsing some lists with data to string
 abilities = ', '.join(
     ability['ability']['name'] for ability in pokemon_data["abilities"]
 )
@@ -25,6 +29,7 @@ types = ', '.join(
     the_type['type']['name'] for the_type in pokemon_data["types"]
 )
 
+# Displaying collected information
 print(f'The name of this pokemon is {pokemon_data["name"]}')
 print(f'It\'s base experience is {pokemon_data["base_experience"]}')
 print(f'It\'s height is {pokemon_data["height"]} and weight is {pokemon_data["weight"]}')
